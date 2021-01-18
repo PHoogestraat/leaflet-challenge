f235be3b1616704fbe5c716f853d017793b47f1f
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2021-01-18 12:36:52 -0500

    can't figure out json

--- Leaflet_step_2/static/js/logic.js
+++ Leaflet_step_2/static/js/logic.js
@@ -32,27 +32,35 @@ var myMap = L.map("map", {
 
 
 // Country data 
-var countries = [];
+var coord = [];
 
 // significant earthquakes past 30 days
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
-// // Perform a GET request to the query URL   **** creates anyonmos function to store json data as data.features
+// Perform a GET request to the query URL   **** creates anyonmos function to store json data as data.features
 d3.json(queryUrl, function(data) {
+  var coord = [];
   console.log(data.features);
-  console.log(data.features.geometry.coordinates);
-  circlePlot(data.features);
+  
+  //console.log(data.features.geometry.coordinates);
+  
+  var coord = data.features
+  console.log("coord test" + coord);
+  console.log(coord);
+  //console.log(coord.Feature.properties)
+
+
+  circlePlot(coord);
+
 });
 
-
-// var countries = d3.json(queryUrl, function(data) {
-//   console.log(data.features);
-//   //console.log(data.features.geometry.coordinates);    Why does this not generate the percise cordiantes?
-//   console.log(`earthquake: ${countries}`);              // Why does this not produce the same as line 42
-//   console.log(countries);              // Why does this not produce the same as line 42
-//   //circleFeatures(data.features);
-// });
-
+        // var countries = d3.json(queryUrl, function(data) {
+        //   console.log(data.features);
+        //   //console.log(data.features.geometry.coordinates);    Why does this not generate the percise cordiantes?
+        //   console.log(`earthquake: ${countries}`);              // Why does this not produce the same as line 42
+        //   console.log(countries);              // Why does this not produce the same as line 42
+        //   //circleFeatures(data.features);
+        // });
 
 
 
@@ -62,31 +70,35 @@ d3.json(queryUrl, function(data) {
 
 
 
-//**************************************************************************************** */
+
+  //**************************************************************************************** */
   // Loop through the cities array and create one marker for each city object
 
-function circlePlot(feature) {
+function circlePlot(earthquakeData) {
 
-    console.log(feature);
-    console.log(features.geometry.coordinate);
+    console.log(earthquakeData);
+    console.log(earthquakeData.features.geometry.coordinates);
+    //console.log(Object.values(feature));
+  
 };
-    //console.log(`countries: ${countries}`);
+
+
     // function(countries){
     //     for (var i = 0; i < countries.length; i++) {
             
     //         // Conditionals for countries points
     //         var color = "";
-    //         if (countries[i].points > 200) {
+    //         if (countries[i].points > 4) {
+    //           color = "Red";
+    //         }
+    //         else if (countries[i].points > 3) {
     //           color = "yellow";
     //         }
-    //         else if (countries[i].points > 100) {
+    //         else if (countries[i].points > 2) {
     //           color = "blue";
     //         }
-    //         else if (countries[i].points > 90) {
-    //           color = "green";
-    //         }
     //         else {
-    //           color = "red";
+    //           color = "white";
     //         }
           
     //         // Add circles to map
